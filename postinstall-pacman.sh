64e7e1d8926addb2dc2fcf9554db4948ab7dfa12
João Paulo <79981086+JoaoPaulo-creator@users.noreply.github.com>
2021-08-31 00:37:59 -0300

    Update postinstall-pacman.sh

--- postinstall-pacman.sh
+++ postinstall-pacman.sh
@@ -5,5 +5,10 @@ sudo pacman -Syyu --noconfirm
 sudo pacman -S brave-browser gedit neofetch --noconfirm
 sudo pamac update && sudo pamac upgrade --no-confirm
 
-echo "Finished"
+sudo pamac install dpkg --no-confirm
+sudo pamac update
+sudo pamac clean
 
+flatpak install Spotify -y
+
+echo "Finished"
