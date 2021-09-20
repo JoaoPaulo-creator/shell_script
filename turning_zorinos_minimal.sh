1759be63c4c1673c9f4cc23378c60c1d846cbb73
João Paulo <79981086+JoaoPaulo-creator@users.noreply.github.com>
2021-09-20 19:37:43 -0300

    Update turning_zorinos_minimal.sh

--- turning_zorinos_minimal.sh
+++ turning_zorinos_minimal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt remove gnome-remote-desktop gnome-sound-recorder gnome-mines quadrapassel libreoffice-* gnome-weather -y
-sudo apt autoremove evolution -y
+sudo apt remove gnome-remote-desktop gnome-sound-recorder gnome-mines quadrapassel libreoffice-* gnome-weather evolution -y
+sudo apt autoremove -y
 
 sudo apt update && sudo apt upgrade -y
