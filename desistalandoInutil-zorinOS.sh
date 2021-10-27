f5fc4b43342cca201d2f4458b0fff2cbfc3aa132
João Paulo <79981086+JoaoPaulo-creator@users.noreply.github.com>
2021-10-27 18:25:26 -0300

    Update desistalandoInutil-zorinOS.sh

--- desistalandoInutil-zorinOS.sh
+++ desistalandoInutil-zorinOS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt remove gnome-remote-desktop gnome-sound-recorder gnome-mines quadrapassel libreoffice-* gnome-weather evolution gnome-sudoku gnome-maps gnome-mahjongg gnome-todo zorin-connect aisleriot gnome-tour -y
+sudo apt remove gnome-remote-desktop gnome-sound-recorder gnome-mines quadrapassel libreoffice-* gnome-weather evolution gnome-sudoku gnome-maps gnome-mahjongg gnome-todo zorin-connect aisleriot gnome-tour brasero remmina -y
 sudo apt autoremove && sudo apt autoclean
 
 sudo apt update && sudo apt upgrade -y
